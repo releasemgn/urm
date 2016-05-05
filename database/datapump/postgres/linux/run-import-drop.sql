1a1974bb7d3ce3c73652bbbb9202de81344a724f
Vladimir Savchik <vsavchik@gmail.com>
2016-05-05 13:39:11 +0300

    push updates

---
+++ database/datapump/postgres/linux/run-import-drop.sql
@@ -0,0 +1,12 @@
+DO $$
+DECLARE
+	sname RECORD;
+BEGIN
+	FOR sname IN SELECT schema_name FROM information_schema.schemata LOOP
+		if sname.schema_name NOT LIKE 'pg_%' and sname.schema_name <> 'information_schema' then
+			execute 'drop schema ' || sname.schema_name || ' cascade';
+		end if;
+	END LOOP;
+
+	execute 'create schema public';
+END $$;
