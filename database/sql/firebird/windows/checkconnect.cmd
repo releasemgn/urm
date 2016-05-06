1b92e4e7dfaaa35d7f05b61fb28d3f9ec968ecfa
Vladimir Savchik <vsavchik@gmail.com>
2016-05-06 15:26:59 +0300

    push updates

--- database/sql/firebird/windows/checkconnect.cmd
+++ database/sql/firebird/windows/checkconnect.cmd
@@ -1,2 +1,6 @@
-echo not ready
+echo show database; | %FBPATH%\isql -q -u %URMDB_USER% -p %URMDB_PWD% "%URMDB_DBHOST%:%URMDB_DBNAME%"
+SET status=%errorlevel%
+echo status=%status%
+
+if %status% == 0 exit /B 0
 exit /B 1
