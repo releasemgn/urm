b940f7d37c7d4161cafc485322c2b4bec25bc227
Vladimir Savchik <vsavchik@gmail.com>
2016-06-21 14:32:15 +0300

    push updates

--- bin/urm.cmd
+++ bin/urm.cmd
@@ -13,9 +13,17 @@ if "%C_URM_MODE%" == "main" (
 )
 
 if "%C_CONTEXT_URMSERVER%" == "" (
-	set JAVACP=master/bin/urms.jar;master/lib/jna-4.1.0.jar
+	set JAVACP=master/bin/urms.jar;master/lib/jmxtools-1.2.1.jar;master/lib/jna-4.1.0.jar
 ) else (
-	set JAVACP=master/bin/urmc.jar
+	set JAVACP=master/bin/urmc.jar;master/lib/jmxtools-1.2.1.jar
+)
+
+set x=0
+if "%1" == "-trace" set x=1
+if "%2" == "-trace" set x=1
+if "%3" == "-trace" set x=1
+if "%x" == "1" (
+	echo run: java -cp %JAVACP% -Duser.language=ru -Durm.mode=%C_URM_MODE% -Durm.os=windows -Durm.installpath=%INSTALL_PATH% -Durm.server=%C_CONTEXT_URMSERVER% -Durm.product=%C_CONTEXT_PRODUCT% -Durm.build=%C_CONTEXT_VERSIONMODE% -Durm.env=%C_CONTEXT_ENV% -Durm.dc=%C_CONTEXT_DC% %C_UMR_CLASS% %*
 )
 
 java -cp %JAVACP% -Duser.language=ru -Durm.mode=%C_URM_MODE% -Durm.os=windows -Durm.installpath=%INSTALL_PATH% -Durm.server=%C_CONTEXT_URMSERVER% -Durm.product=%C_CONTEXT_PRODUCT% -Durm.build=%C_CONTEXT_VERSIONMODE% -Durm.env=%C_CONTEXT_ENV% -Durm.dc=%C_CONTEXT_DC% %C_UMR_CLASS% %*
--- bin/urm.sh
+++ bin/urm.sh
@@ -13,9 +13,13 @@ else
 fi
 
 if [ "$C_CONTEXT_URMSERVER" = "" ]; then
-	JAVACP=master/bin/urms.jar:master/lib/jna-4.1.0.jar
+	JAVACP=master/bin/urms.jar:master/lib/jmxtools-1.2.1.jar:master/lib/jna-4.1.0.jar
 else
-	JAVACP=master/bin/urmc.jar
+	JAVACP=master/bin/urmc.jar:master/lib/jmxtools-1.2.1.jar
+fi
+
+if [ "$1" = "-trace" ] || [ "$2" = "-trace" ] || [ "$3" = "-trace" ]; then
+	echo run: java -cp $JAVACP -Duser.language=ru -Dfile.encoding=utf-8 -Durm.mode=$C_URM_MODE -Durm.os=linux -Durm.installpath=$INSTALL_PATH -Durm.server=$C_CONTEXT_URMSERVER -Durm.product=$C_CONTEXT_PRODUCT -Durm.build=$C_CONTEXT_VERSIONMODE -Durm.env=$C_CONTEXT_ENV -Durm.dc=$C_CONTEXT_DC $C_UMR_CLASS "$@"
 fi
 
 java -cp $JAVACP -Duser.language=ru -Dfile.encoding=utf-8 -Durm.mode=$C_URM_MODE -Durm.os=linux -Durm.installpath=$INSTALL_PATH -Durm.server=$C_CONTEXT_URMSERVER -Durm.product=$C_CONTEXT_PRODUCT -Durm.build=$C_CONTEXT_VERSIONMODE -Durm.env=$C_CONTEXT_ENV -Durm.dc=$C_CONTEXT_DC $C_UMR_CLASS "$@"
